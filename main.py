0019a84636abf466f8b4d62072596d150972f919
haduchau321 <83698436+haduchau321@users.noreply.github.com>
2021-05-08 22:31:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
-# import uvicorn
 import buff_live
-
-
-
 app = FastAPI()
 
 @app.get('/get_data/cookies={cookies}')
@@ -14,11 +10,7 @@ async def home(cookies:str):
 
 @app.get('/Buff/id_sy={id_sy}/id_post={id_post}/id_video={id_video}/cookies={cookies}')
 async def buff(id_sy:str,id_post:str,id_video:str,cookies:str):
-    print(id_sy,id_post,id_video,cookies)
     b = buff_live.main_buff(cookies)
     out = b.buff(id_sy,id_post,id_video)
     return out
 
-# uvicorn.run(app)
-
-#  {"id_sy":"AQE5yR7GMR_37vQ:AQFZ_gcVHE0wO-M","id_post":"22412"}
